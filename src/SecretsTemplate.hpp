a3a95a7165aed9f06e507139c6ebd91d1db00ec8
Robin Singh Nanda <nanda.robin.singh@gmail.com>
2020-10-30 19:12:35 +0530

    Added template file for Secrets.hpp

---
+++ src/SecretsTemplate.hpp
@@ -0,0 +1,14 @@
+#ifndef __SECRETS_H__
+#define __SECRETS_H__
+
+
+//Copy this file to Secrets.hpp and change the settings to your setup
+
+#define WIFI_SSID "######"
+#define WIFI_PASSWORD "########"
+
+#define HOME_ASSITANT_WEBSOCKET "wss://<homeassistnat-host>/api/websocket"
+#define HOME_ASSITANT_AUTH_MESSAGE "{\"type\":\"auth\",\"access_token\":\"<accesstoken>\"}"
+
+
+#endif // __SECRETS_H__
